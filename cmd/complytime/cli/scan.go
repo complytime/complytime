1f6b2522b60b53bc22709f067a7367d56b2bb6d1
Jennifer Power <barnabei.jennifer@gmail.com>
2024-12-17 11:28:29 -0500

    Add the scan subcommand
    The scan subcommand is added to the ComplyTime CLI with an internal
    complytime package to support the configuration of the C2P Plugin
    Manager and generation of the configuration for ComplyTime use.
    
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>

---
+++ cmd/complytime/cli/scan.go
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package cli
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/oscal-compass/compliance-to-policy-go/v2/framework"
+	"github.com/spf13/cobra"
+
+	"github.com/complytime/complytime/cmd/complytime/option"
+	"github.com/complytime/complytime/internal/complytime"
+)
+
+// scanOptions defined options for the scan subcommand.
+type scanOptions struct {
+	*option.Common
+	assessmentPlanPath string
+}
+
+// scanCmd creates a new cobra.Command for the version subcommand.
+func scanCmd(common *option.Common) *cobra.Command {
+	scanOpts := &scanOptions{Common: common}
+	return &cobra.Command{
+		Use:          "scan [flags] <assessment_plan_path>",
+		Short:        "Scan environment with assessment plan",
+		Example:      "complytime scan assessment-plan.json",
+		SilenceUsage: true,
+		Args:         cobra.ExactArgs(1),
+		PreRun: func(cmd *cobra.Command, args []string) {
+			scanOpts.assessmentPlanPath = filepath.Clean(args[0])
+		},
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			return runScan(cmd, scanOpts)
+		},
+	}
+}
+
+func runScan(cmd *cobra.Command, opts *scanOptions) error {
+	// Adding this message to the user for now because assessment Plans are unused
+	_, _ = fmt.Fprintf(opts.Out, "OSCAL Assessment Plans are not supported yet. The file %s will not be used\n", opts.assessmentPlanPath)
+
+	// Create the application directory if it does not exist
+	appDir, err := complytime.NewApplicationDirectory(true)
+	if err != nil {
+		return err
+	}
+
+	cfg, err := complytime.Config(appDir)
+	if err != nil {
+		return err
+	}
+	manager, err := framework.NewPluginManager(cfg)
+	if err != nil {
+		return fmt.Errorf("error initializing plugin manager: %w", err)
+	}
+	plugins, err := manager.LaunchPolicyPlugins()
+	if err != nil {
+		return err
+	}
+	allResults, err := manager.AggregateResults(cmd.Context(), plugins)
+	if err != nil {
+		return err
+	}
+	manager.Clean()
+	for _, result := range allResults {
+		_, _ = fmt.Fprintf(opts.Out, "Result:\n%s", result)
+	}
+	return nil
+}
---
+++ internal/complytime/configuration_test.go
@@ -0,0 +1,43 @@
+package complytime
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestApplicationDirectory(t *testing.T) {
+	tmpDir := os.TempDir()
+	appDir, err := newApplicationDirectory(tmpDir, false)
+	require.NoError(t, err)
+
+	expectedAppDir := filepath.Join(tmpDir, "complytime")
+	expectedPluginDir := filepath.Join(tmpDir, "complytime", "plugins")
+	expextedBundleDir := filepath.Join(tmpDir, "complytime", "bundles")
+
+	require.Equal(t, expectedAppDir, appDir.AppDir())
+	require.Equal(t, expectedPluginDir, appDir.PluginDir())
+	require.Equal(t, expextedBundleDir, appDir.BundleDir())
+	require.Equal(t, []string{expectedAppDir, expectedPluginDir, expextedBundleDir}, appDir.Dirs())
+
+	appDir, err = newApplicationDirectory(tmpDir, true)
+	require.NoError(t, err)
+	_, err = os.Stat(appDir.AppDir())
+	require.NoError(t, err)
+	_, err = os.Stat(appDir.PluginDir())
+	require.NoError(t, err)
+	_, err = os.Stat(appDir.BundleDir())
+	require.NoError(t, err)
+}
+
+func TestFindComponentDefinitions(t *testing.T) {
+	componentDefinitions, err := FindComponentDefinitions("testdata/bundles")
+	require.NoError(t, err)
+	require.Len(t, componentDefinitions, 1)
+
+	_, err = FindComponentDefinitions("testdata/")
+	require.ErrorIs(t, err, ErrNotComponentDefinitionsFound)
+
+}

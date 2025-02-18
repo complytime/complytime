0803bc03913ab1546021e17a542fc96eb5f070af
George Vauter <gvauter@redhat.com>
2025-02-18 15:34:59 -0500

    feat: add charm logger in cli package
    Signed-off-by: George Vauter <gvauter@redhat.com>

--- cmd/complytime/cli/root.go
+++ cmd/complytime/cli/root.go
@@ -3,13 +3,27 @@
 package cli
 
 import (
-	"github.com/spf13/cobra"
+	"os"
 
 	"github.com/complytime/complytime/cmd/complytime/option"
+	"github.com/complytime/complytime/pkg/log"
+	"github.com/hashicorp/go-hclog"
+	"github.com/spf13/cobra"
 )
+
+var logger hclog.Logger
+
+func enableDebug(opts *option.Common) {
+	if opts.Debug {
+		logger.SetLevel(hclog.Debug)
+	}
+}
 
 // New creates a new cobra.Command root for ComplyTime
 func New() *cobra.Command {
+
+	logger = log.NewLogger(os.Stdout)
+
 	cmd := &cobra.Command{
 		Use:           "complytime [command]",
 		SilenceErrors: true,

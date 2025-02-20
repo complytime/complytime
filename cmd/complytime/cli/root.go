a286214bc0211fdce83e72a5ad56e538c7081276
Hannah Braswell <135030802+hbraswelrh@users.noreply.github.com>
2025-02-20 16:17:47 -0500

    feat: logger statements added to generate command
    Signed-off-by: Hannah Braswell <hbraswel@redhat.com>

--- cmd/complytime/cli/root.go
+++ cmd/complytime/cli/root.go
@@ -41,7 +41,7 @@ func New() *cobra.Command {
 	cmd.AddCommand(
 		versionCmd(&opts),
 		scanCmd(&opts, logger),
-		generateCmd(&opts),
+		generateCmd(&opts, logger),
 		planCmd(&opts, logger),
 		listCmd(&opts, logger),
 	)

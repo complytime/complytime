a99a2594e3039b7fd9bf56ac7777b8cba439f70c
Hannah Braswell <hbraswel@redhat.com>
2025-04-09 16:18:07 -0400

    feat: introducing flags for assessment plan editing

--- cmd/complytime/option/common.go
+++ cmd/complytime/option/common.go
@@ -34,7 +34,22 @@ type ComplyTime struct {
 	UserWorkspace string
 }
 
+type UpdatePlan struct {
+	Controls   map[string]string
+	Rules      map[string]string
+	Parameters map[string]string
+	Config     string
+}
+
 // BindFlags populate ComplyTime options from user-specified flags.
 func (o *ComplyTime) BindFlags(fs *pflag.FlagSet) {
 	fs.StringVarP(&o.UserWorkspace, "workspace", "w", ".", "workspace to use for artifact generation")
+}
+func (o *UpdatePlan) BindFlags(fs *pflag.FlagSet) {
+	fs.StringP("config", "c", "./config/complytime", "location of complytime bundles and controls in home dir")
+	fs.StringSliceP("update-controls", "u", []string{}, "update controls from 'ready to assess' to waived")
+	fs.StringSlice("exclude-controls", []string{}, "controls to be excluded from assessment-plan.json")
+	fs.StringSlice("update-rules", []string{}, "update rules from 'ready to assess' to waived")
+	fs.StringSlice("exclude-rules", []string{}, "rules to be excluded from assessment-plan.json")
+	fs.StringSlice("update-parameters", []string{}, "update parameters from 'ready to assess' to waived")
 }

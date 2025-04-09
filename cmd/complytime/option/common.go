86cca2df3ee72fe31b9905472ad1092e7aa12438
Hannah Braswell <hbraswel@redhat.com>
2025-04-09 16:18:07 -0400

    feat: introducing flags for assessment plan editing
    Signed-off-by: Hannah Braswell <hbraswel@redhat.com>

--- cmd/complytime/option/common.go
+++ cmd/complytime/option/common.go
@@ -34,7 +34,26 @@ type ComplyTime struct {
 	UserWorkspace string
 }
 
+type UpdatePlan struct {
+	Controls   map[string]string
+	Rules      map[string]string
+	Parameters map[string]string
+	Config     string
+	DryRun     bool
+}
+
 // BindFlags populate ComplyTime options from user-specified flags.
 func (o *ComplyTime) BindFlags(fs *pflag.FlagSet) {
 	fs.StringVarP(&o.UserWorkspace, "workspace", "w", ".", "workspace to use for artifact generation")
+}
+
+// BindFlags populate UpdatePlan options from user specified flags.
+func (o *UpdatePlan) BindFlags(fs *pflag.FlagSet) {
+	fs.StringP("config", "c", "./config/complytime", "location of complytime bundles and controls in home dir")
+	fs.Bool("dry-run", false, "print table of assessment-plan components to standard output")
+	fs.StringSliceP("update-controls", "u", []string{}, "update control(s) status from 'ready to assess' to 'waived'")
+	fs.StringSlice("exclude-controls", []string{}, "control(s) to exclude from assessment-plan.json")
+	fs.StringSlice("update-rules", []string{}, "update rule(s) status from 'ready to assess' to 'waived'")
+	fs.StringSlice("exclude-rules", []string{}, "rule(s) to exclude from assessment-plan.json")
+	fs.StringSlice("update-parameters", []string{}, "update parameter(s) status from 'ready to assess' to 'waived'")
 }

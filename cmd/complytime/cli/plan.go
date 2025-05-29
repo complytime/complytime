ff0fdb5a4fd3df1ee43fb8a5a7d0466a8a20921a
Jennifer Power <barnabei.jennifer@gmail.com>
2025-05-29 11:08:35 -0400

    fix: apply fixes to broken build
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>

--- cmd/complytime/cli/plan.go
+++ cmd/complytime/cli/plan.go
@@ -21,9 +21,6 @@ import (
 
 const assessmentPlanLocation = "assessment-plan.json"
 
-// TODO: `assessmentPlanFilterLocation` is generic, but can be input based on the user preference
-const assessmentPlanFilterLocation = "config.yml"
-
 // PlanOptions defines options for the "plan" subcommand
 type planOptions struct {
 	*option.Common
